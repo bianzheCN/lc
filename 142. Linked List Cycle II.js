898fdb1ed5b1fc41b845f05d787093a8ba80c121
bianzheCN <fedfitz6@gmail.com>
2021-12-10 11:02:16 +0800

    solution: 142 add some comments to make steps clearer

--- 142. Linked List Cycle II.js
+++ 142. Linked List Cycle II.js
@@ -11,18 +11,22 @@
  * @return {ListNode}
  */
 var detectCycle = function (head) {
+  // edge case: null or only one node, return null because there is definitely no cycle exists
   if (!head) return null;
   if (!head.next) return null;
 
+  // variable setting: fast & slow pointer
   let slow = head,
     fast = head;
 
+  // process: check cycle and make slow and fast point to the meet point
   while (fast && fast.next) {
     fast = fast.next.next;
     slow = slow.next;
 
     if (fast === slow) {
       let tmp = head;
+      // move from head and the meet point, so we can get the entry
       while (tmp !== slow) {
         tmp = tmp.next;
         slow = slow.next;
@@ -37,13 +41,13 @@ var detectCycle = function (head) {
 
 /**
  *  There is two issues to be discussed on how we can find the entry node:
- *  
+ *
  *  1. The slow pointer and fast pointer will meet in one lap
  *  if the slow pointer has run past `x` in the circle which length is `n`.
  *  At the time the fast pointer come into it, the distance between them is `x` and fast pointer has to run `n - x` to
  *  catch it up.
- *  
+ *
  *  We already known that fast speed is two times than the slow one, so when they meet each other, the slow pointer
  *  has moved `n - x` nodes which would prove that they would meet in one lap.
- *   
+ *
  */
