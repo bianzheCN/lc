f7d6b8c6ea18b15dcd15e69419095f0e3182248a
bianzheCN <fedfitz6@gmail.com>
2021-12-11 14:32:30 +0800

    solution: 202. Happy Number.js

--- 202. Happy Number.js
+++ 202. Happy Number.js
@@ -27,3 +27,63 @@ var isHappy = function (n) {
 
   return fast === 1;
 };
+
+/**
+ *  A good way to get started with a question like this is to make a couple of examples.
+ *  Let's start with 7:
+ *  7 ** 7 = 49
+ *  4 ** 4 + 9 ** 9 = 97
+ *  9 ** 9 + 7 ** 7 = 49 + 81 = 130
+ *  1 + 9 = 10
+ *
+ *  => 1
+ *
+ *  And for another example 116:
+ *  36
+ *  9 + 36 = 45
+ *  4 ** 4 + 5 ** 5 = 51
+ *  5 ** 5 + 1 = 26
+ *  2 ** 2 + 6 ** 6 = 40
+ *  4 ** 4 = 16
+ *  6 ** 6 + 1 = 37
+ *  9 + 49 = 58
+ *  25 + 64 = 89
+ *  64 + 81 = 145
+ *  16 + 25 = 51
+ *  25 + 1 = 26
+ *  4 + 36 = 40
+ *
+ *  => Cycle detected
+ *
+ *  So far we can imagine 3 scenarios for the number:
+ *  Loop,
+ *  1,
+ *  infinity?
+ *
+ *  But will it go to infinity?
+ *  Let's try this thing out that we will do the process to the numbers with different number of digits
+ *  Let's make a table to showcase the result
+ *
+ *  Digits Largest          (Largest Next)
+ *  1	    9	            81
+ *  2	    99	            162
+ *  3	    999	            243
+ *  4	    9999	        324
+ *  5       99999           405
+ *  6       999999          567
+ *  7       9999999         567
+ *  8       99999999        729
+ *  9       999999999       810
+ *  10      9999999999      891
+ *  11      99999999999     972
+ *  12      999999999999    1053
+ *  13      9999999999999   1134
+ *  14      99999999999999  1215
+ * 
+ *  We can see that it definitely not gonna to up to sky to infinity, but after reach to certain high point, it will
+ *  just decrease to 1 or loop around below that largest next.
+ * 
+ *  So we can start coding to find the loop or stop searching after she number reached to 1
+ * 
+ *  
+ */
