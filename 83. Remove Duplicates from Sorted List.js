48cd11da801a5d012f0e97514b6a379ac2f57a1c
bianzheCN <fedfitz6@gmail.com>
2021-12-13 18:31:56 +0800

    solution: 83. Remove Duplicates from Sorted List.js

---
+++ 83. Remove Duplicates from Sorted List.js
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+  if (!head) return null;
+
+  const dummyHead = new ListNode(null, head);
+  let slow = dummyHead,
+    fast = dummyHead;
+
+  while (fast) {
+    if (slow.val !== fast.val) {
+      slow.next = fast;
+      slow = fast;
+    }
+
+    fast = fast.next;
+  }
+  slow.next = fast;
+
+  return dummyHead.next;
+};
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+  if (!head) return null;
+
+  const dummyHead = new ListNode(null, head);
+  let cur = dummyHead;
+
+  while (cur && cur.next) {
+    if (cur.val === cur.next.val) {
+      cur.next = cur.next.next;
+    } else {
+      cur = cur.next;
+    }
+  }
+
+  return dummyHead.next;
+};
