ce8bf44441bc91909e08dc31f10267f3dfc9c682
bianzheCN <fedfitz6@gmail.com>
2021-12-14 10:34:33 +0800

    solution: 86. Partition List

---
+++ 86. Partition List.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+var partition = function (head, x) {
+  let small = new ListNode(0);
+  const smallHead = small;
+  let large = new ListNode(0);
+  const largeHead = large;
+
+  while (head !== null) {
+    if (head.val < x) {
+      small.next = head;
+      small = small.next;
+    } else {
+      large.next = head;
+      large = large.next;
+    }
+    head = head.next;
+  }
+
+  large.next = null;
+  small.next = largeHead.next;
+
+  return smallHead.next;
+};
