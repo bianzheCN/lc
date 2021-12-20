71417fcb316178e26db243d613a684264bda750f
bianzheCN <fedfitz6@gmail.com>
2021-12-20 10:50:52 +0800

    feat: Validate Stack Sequences

--- 946. Validate Stack Sequences.js
+++ 946. Validate Stack Sequences.js
@@ -21,3 +21,23 @@ var validateStackSequences = function (pushed, popped) {
 
   return true
 }
+
+/**
+ * @param {number[]} pushed
+ * @param {number[]} popped
+ * @return {boolean}
+ */
+var validateStackSequences = function (pushed, popped) {
+  const stack = [],
+    cur = 0
+
+  for (item of pushed) {
+    stack.push(item)
+    while (stack[stack.length - 1] === popped[cur] && stack.length) {
+      stack.pop()
+      cur++
+    }
+  }
+
+  return !stack.length
+}
