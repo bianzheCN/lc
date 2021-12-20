fa036c1aaf0937502be6f5e8e968fed079c35761
bianzheCN <fedfitz6@gmail.com>
2021-12-20 10:51:24 +0800

    feat: Valid Parenthese

--- 20. Valid Parentheses.js
+++ 20. Valid Parentheses.js
@@ -3,21 +3,46 @@
  * @return {boolean}
  */
 var isValid = function (s) {
-  let left = ["(", "{", "["];
-  let right = [")", "}", "]"];
-  const stack = [];
+  let left = ["(", "{", "["]
+  let right = [")", "}", "]"]
+  const stack = []
 
   for (const c of s) {
     if (left.includes(c)) {
-      stack.push(c);
+      stack.push(c)
     } else {
-      const leftChar = stack.pop();
-      const index = left.indexOf(leftChar);
-      if (right[index] !== c) return false;
+      const leftChar = stack.pop()
+      const index = left.indexOf(leftChar)
+      if (right[index] !== c) return false
     }
   }
 
-  if (stack.length) return false;
+  if (stack.length) return false
 
-  return true;
-};
+  return true
+}
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  const stack = []
+
+  for (let c of s) {
+    if (c === "(" || c === "[" || c === "{") {
+      stack.push(c)
+    } else {
+      const poppedItem = stack.pop()
+      if (c === "]") {
+        if (poppedItem !== "[") return false
+      } else if (c === ")") {
+        if (poppedItem !== "(") return false
+      } else {
+        if (poppedItem !== "{") return false
+      }
+    }
+  }
+
+  return !stack.length
+}
