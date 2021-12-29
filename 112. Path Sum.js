0b5f9dbcf9dadc7d93991270833688a52f5a9f04
bianzheCN <fedfitz6@gmail.com>
2021-12-29 14:03:25 +0800

    feat: Path Sum

--- 112. Path Sum.js
+++ 112. Path Sum.js
@@ -22,34 +22,42 @@ var hasPathSum = function (root, targetSum) {
   )
 }
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
 var hasPathSum = function (root, targetSum) {
   // BFS
-  if (!root) {
-    return false
-  }
+  if (!root) return false
 
-  // BFS法 创建两个数组 一个记录所有节点 一个记录路径和
-  const queue = []
-  const res = []
-  queue.push(root)
-  res.push(root.val)
+  const q = [root]
+  const v = [root.val]
 
-  // 进入BFS
-  while (queue.length) {
-    const top = queue.pop()
-    const temp = res.pop()
-    // 如果遍历到叶子节点处时 路径和=targetSum 则返回true
-    if (top.left === null && top.right === null) {
-      if (temp === targetSum) return true
+  while (q.length) {
+    const node = q.pop()
+    const val = v.pop()
+
+    if (!node.left && !node.right) {
+      if (val === targetSum) return true
     }
-    // 如层序遍历一般更新queue与路径和数组
-    if (top.left) {
-      queue.push(top.left)
-      res.push(temp + top.left.val)
+
+    if (node.left) {
+      q.push(node.left)
+      v.push(val + node.left.val)
     }
-    if (top.right) {
-      queue.push(top.right)
-      res.push(temp + top.right.val)
+
+    if (node.right) {
+      q.push(node.right)
+      v.push(val + node.right.val)
     }
   }
 
