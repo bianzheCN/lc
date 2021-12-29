d5a8eeb02feb7b1d3416ca486b10a7276c56a447
bianzheCN <fedfitz6@gmail.com>
2021-12-29 22:50:33 +0800

    feat: Count Complete Tree Nodes

--- 222. Count Complete Tree Nodes.js
+++ 222. Count Complete Tree Nodes.js
@@ -10,26 +10,26 @@
  * @param {TreeNode} root
  * @return {number}
  */
- var countNodes = function(root) {
-    let ret = 1
-    if (!root) return 0
+var countNodes = function (root) {
+  let ret = 1
+  if (!root) return 0
 
-    const go = (cur) => {
-        if (cur.left) {
-            ret++
-            go(cur.left)
-        }
-
-        if (cur.right) {
-            ret++
-            go(cur.right)
-        }
+  const go = (cur) => {
+    if (cur.left) {
+      ret++
+      go(cur.left)
     }
 
-    go(root)
+    if (cur.right) {
+      ret++
+      go(cur.right)
+    }
+  }
 
-    return ret
-};
+  go(root)
+
+  return ret
+}
 
 /**
  * Definition for a binary tree node.
@@ -43,6 +43,33 @@
  * @param {TreeNode} root
  * @return {number}
  */
- var countNodes = function(root) {
-    // exploit complete tree
-};
+var countNodes = function (root) {
+  // exploit complete tree
+  if (root == null) {
+    // 递归的出口
+    return 0
+  }
+
+  let lH, // 两侧高度
+    rH = 0
+  let lNode, // 两个指针
+    rNode = root
+
+  while (lNode) {
+    // 计算左侧高度
+    lH++
+    lNode = lNode.left
+  }
+  while (rNode) {
+    // 计算右侧高度
+    rH++
+    rNode = rNode.right
+  }
+  if (lH == rH) {
+    // 当前子树是满二叉树，返回出节点数
+    return 2 ** lH - 1 // 左移 n 位就是乘以2的n次方
+  }
+
+  // 当前子树不是完美二叉树，只是完全二叉树，递归处理左右子树
+  return 1 + countNodes(root.left) + countNodes(root.right)
+}
