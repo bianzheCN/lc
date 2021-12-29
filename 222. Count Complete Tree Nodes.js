ac034bdc120ec027814bbaa1c9874b08a8284c94
bianzheCN <fedfitz6@gmail.com>
2021-12-29 22:08:59 +0800

    feat: Count Complete Tree Nodes

---
+++ 222. Count Complete Tree Nodes.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var countNodes = function(root) {
+    let ret = 1
+    if (!root) return 0
+
+    const go = (cur) => {
+        if (cur.left) {
+            ret++
+            go(cur.left)
+        }
+
+        if (cur.right) {
+            ret++
+            go(cur.right)
+        }
+    }
+
+    go(root)
+
+    return ret
+};
