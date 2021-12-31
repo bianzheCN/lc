2c1fee8065863ce2cf15a9f18f95b9857523b9d3
bianzheCN <fedfitz6@gmail.com>
2021-12-31 20:04:35 +0800

    feat: Binary Tree Preorder Traversal

--- 144. Binary Tree Preorder Traversal.js
+++ 144. Binary Tree Preorder Traversal.js
@@ -11,7 +11,8 @@
  * @return {number[]}
  */
 var preorderTraversal = function (root) {
-    const ret = []
+  // recursion
+  const ret = []
   if (!root) return ret
 
   const dfs = (root) => {
@@ -22,5 +23,37 @@ var preorderTraversal = function (root) {
   }
 
   dfs(root)
+  return ret
+}
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function (root) {
+  // iteration
+  const ret = []
+  if (!root) ret
+  const stack = []
+  let cur = root
+
+  while (cur || stack.length) {
+    while (cur) {
+      ret.push(cur.val)
+      stack.push(cur)
+      cur = cur.left
+    }
+
+    cur = stack.pop().right
+  }
+
   return ret
 }
