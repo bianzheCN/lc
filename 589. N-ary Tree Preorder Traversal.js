6c87b5a9beace40701c5d4cae56cbed13e450c57
bianzheCN <fedfitz6@gmail.com>
2021-12-31 20:15:34 +0800

    feat: N-ary Tree Preorder Traversal

--- 589. N-ary Tree Preorder Traversal.js
+++ 589. N-ary Tree Preorder Traversal.js
@@ -11,6 +11,7 @@
  * @return {number[]}
  */
 var preorder = function (root) {
+  // recursion
   const output = []
   if (!root) return output
 
@@ -24,4 +25,32 @@ var preorder = function (root) {
   traverse(root)
 
   return output
+}
+
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number[]}
+ */
+var preorder = function (root) {
+  // iteration
+  if (!root) return []
+  const stack = [root]
+  const ret = []
+
+  while (stack.length) {
+    const node = stack.pop()
+    ret.push(node.val)
+
+    stack.push(...node.children.reverse())
+  }
+
+  return ret
 }
