6b077490929de352e90da09f41b534eedbb05b8a
bianzheCN <fedfitz6@gmail.com>
2022-01-01 22:20:54 +0800

    feat: Binary Tree Cameras

--- 968. Binary Tree Cameras.js
+++ 968. Binary Tree Cameras.js
@@ -1,3 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
 var minCameraCover = function (root) {
   const traversal = (cur) => {
     // 空节点，该节点有覆盖
