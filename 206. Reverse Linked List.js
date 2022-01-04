33408fe41dd1b26c2af3ee65b9c10353855cb508
bianzheCN <fedfitz6@gmail.com>
2022-01-04 21:16:26 +0800

    feat: Reverse Linked List

--- 206. Reverse Linked List.js
+++ 206. Reverse Linked List.js
@@ -10,17 +10,41 @@
  * @return {ListNode}
  */
 var reverseList = function (head) {
-  const dummyHead = new ListNode(null);
-  let cur = dummyHead;
+  const dummyHead = new ListNode()
 
   while (head) {
-    const tmp = head.next;
+    const next = dummyHead.next
+    const next2 = head.next
+    dummyHead.next = head
+    head.next = next
 
-    head.next = cur.next;
-    cur.next = head;
-    cur = tmp;
-    head = tmp;
+    head = next2
   }
 
-  return dummyHead.next;
-};
+  return dummyHead.next
+}
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let prev = null
+  let cur = head
+
+  while (cur) {
+    const tmp = cur.next
+    cur.next = prev
+    prev = cur
+    cur = tmp
+  }
+
+  return prev
+}
