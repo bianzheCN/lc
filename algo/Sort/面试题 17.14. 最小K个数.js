6fc98e08589ba48728fe3a2b40e32c55d9a36c65
bianzheCN <fedfitz6@gmail.com>
2022-01-16 13:38:40 +0800

    algo: sort, 最小 k 个数

---
+++ algo/Sort/面试题 17.14. 最小K个数.js
@@ -0,0 +1,51 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number[]}
+ */
+var smallestK = function (arr, k) {
+  randomizedSelected(arr, 0, arr.length - 1, k)
+  return arr.slice(0, k)
+}
+
+const randomizedSelected = (arr, l, r, k) => {
+  if (l >= r) {
+    return
+  }
+  const pos = randomizedPartition(arr, l, r)
+  const num = pos - l + 1
+  if (k === num) {
+    return
+  } else if (k < num) {
+    randomizedSelected(arr, l, pos - 1, k)
+  } else {
+    randomizedSelected(arr, pos + 1, r, k - num)
+  }
+}
+
+// 基于随机的划分
+const randomizedPartition = (nums, l, r) => {
+  const i = parseInt(Math.random() * (r - l + 1)) + l
+  swap(nums, r, i)
+
+  return partition(nums, l, r)
+}
+
+const partition = (nums, l, r) => {
+  const pivot = nums[r]
+  let i = l - 1
+
+  for (let j = l; j <= r - 1; j++) {
+    if (nums[j] <= pivot) {
+      i = i + 1
+      swap(nums, i, j)
+    }
+  }
+
+  swap(nums, i + 1, r)
+  return i + 1
+}
+
+const swap = (nums, i, j) => {
+  ;[nums[i], nums[j]] = [nums[j], nums[i]]
+}
