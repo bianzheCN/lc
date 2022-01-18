04bf2b8c65c8a4f64a5799d0df02d5788a36b8f6
bianzheCN <fedfitz6@gmail.com>
2022-01-18 20:05:43 +0800

    algo: merge sort, Sort List

--- algo/Sort/148. Sort List.js
+++ algo/Sort/148. Sort List.js
@@ -1,3 +1,4 @@
+// partition
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -72,3 +73,57 @@ function insertSort(arr, l, r) {
     }
   }
 }
+
+// merge sort solution
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var sortList = function (head) {
+  const arr = []
+  let cur = head
+  while (cur) {
+      arr.push(cur.val)
+      cur = cur.next
+  }
+
+  mergeSort(arr, 0, arr.length - 1)
+  const ret = new ListNode()
+  let p = ret
+  for (const x of arr) {
+      p.next = new ListNode(x, null)
+      p = p.next
+  }
+
+  return ret.next
+};
+
+function mergeSort(arr, l, r) {
+  if (l >= r) return
+
+  const mid = (l + r) >> 1, temp = []
+  let p1 = l, p2 = mid + 1, k = 0
+
+  mergeSort(arr, l, mid)
+  mergeSort(arr, mid + 1, r)
+
+  while (p1 <= mid || p2 <= r) {
+      if (p2 > r || (p1 <= mid && arr[p1] <= arr[p2])) {
+          temp[k++] = arr[p1++]
+      } else {
+          temp[k++] = arr[p2++]
+      }
+  }
+
+  for (let i = l; i <= r; i++) {
+      arr[i] = temp[i - l]
+  }
+}
+
