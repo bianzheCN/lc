04c905c56a0aeb5914c5dcc5b61102f06564a5e5
bianzheCN <fedfitz6@gmail.com>
2022-01-18 21:16:54 +0800

    algo: merge sort, Sort List

--- algo/Sort/148. Sort List.js
+++ algo/Sort/148. Sort List.js
@@ -86,44 +86,103 @@ function insertSort(arr, l, r) {
  * @param {ListNode} head
  * @return {ListNode}
  */
- var sortList = function (head) {
+var sortList = function (head) {
   const arr = []
   let cur = head
   while (cur) {
-      arr.push(cur.val)
-      cur = cur.next
+    arr.push(cur.val)
+    cur = cur.next
   }
 
   mergeSort(arr, 0, arr.length - 1)
   const ret = new ListNode()
   let p = ret
   for (const x of arr) {
-      p.next = new ListNode(x, null)
-      p = p.next
+    p.next = new ListNode(x, null)
+    p = p.next
   }
 
   return ret.next
-};
+}
 
 function mergeSort(arr, l, r) {
   if (l >= r) return
 
-  const mid = (l + r) >> 1, temp = []
-  let p1 = l, p2 = mid + 1, k = 0
+  const mid = (l + r) >> 1,
+    temp = []
+  let p1 = l,
+    p2 = mid + 1,
+    k = 0
 
   mergeSort(arr, l, mid)
   mergeSort(arr, mid + 1, r)
 
   while (p1 <= mid || p2 <= r) {
-      if (p2 > r || (p1 <= mid && arr[p1] <= arr[p2])) {
-          temp[k++] = arr[p1++]
-      } else {
-          temp[k++] = arr[p2++]
-      }
+    if (p2 > r || (p1 <= mid && arr[p1] <= arr[p2])) {
+      temp[k++] = arr[p1++]
+    } else {
+      temp[k++] = arr[p2++]
+    }
   }
 
   for (let i = l; i <= r; i++) {
-      arr[i] = temp[i - l]
+    arr[i] = temp[i - l]
   }
 }
 
+//merge sort
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var sortList = function (head) {
+  if (!head || !head.next) return head
+  let n = 0
+  let cur = head
+  while (cur) {
+    cur = cur.next
+    n++
+  }
+
+  return mergeSort(head, n)
+}
+
+function mergeSort(head, n) {
+  if (n <= 1) return head
+
+  const mid = n >> 1,
+    ret = new ListNode()
+  let p = ret,
+    p1 = head,
+    p2 = head
+
+  for (let i = 1; i < mid; i++) {
+    p2 = p2.next
+  }
+  const tmp = p2.next
+  p2.next = null
+  p2 = tmp
+
+  let l1 = mergeSort(head, mid)
+  let l2 = mergeSort(p2, n - mid)
+
+  while (l1 || l2) {
+    if (!l2 || (l1 && l1.val <= l2.val)) {
+      p.next = l1
+      l1 = l1.next
+    } else {
+      p.next = l2
+      l2 = l2.next
+    }
+    p = p.next
+  }
+
+  return ret.next
+}
