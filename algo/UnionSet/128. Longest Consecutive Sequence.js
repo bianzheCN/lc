45ca7ca6a3b9d01fd910876b601c2a46df6b7e51
bianzheCN <fedfitz6@gmail.com>
2022-01-24 21:23:55 +0800

    algo: UnionSet, Longest Consecutive Sequence

--- algo/UnionSet/128. Longest Consecutive Sequence.js
+++ algo/UnionSet/128. Longest Consecutive Sequence.js
@@ -57,6 +57,7 @@ class UnionSet {
 var longestConsecutive = (nums) => {
   const set = new Set(nums) // set存放数组的全部数字
   let max = 0
+
   for (let i = 0; i < nums.length; i++) {
     if (!set.has(nums[i] - 1)) {
       // nums[i]没有左邻居，是序列的起点
@@ -68,6 +69,27 @@ var longestConsecutive = (nums) => {
         count++
       }
       max = Math.max(max, count) // cur不再有右邻居，检查count是否最大
+    }
+  }
+
+  return max
+}
+
+// 笨猪爆破组解法
+var longestConsecutive = (nums) => {
+  let map = new Map()
+  let max = 0
+  for (const num of nums) {
+    // 遍历nums数组
+    if (!map.has(num)) {
+      // 重复的数字不考察，跳过
+      let preLen = map.get(num - 1) || 0 // 获取左邻居所在序列的长度
+      let nextLen = map.get(num + 1) || 0 // 获取右邻居所在序列的长度
+      let curLen = preLen + 1 + nextLen // 新序列的长度
+      map.set(num, curLen) // 将自己存入 map
+      max = Math.max(max, curLen) // 和 max 比较，试图刷新max
+      map.set(num - preLen, curLen) // 更新新序列的左端数字的value
+      map.set(num + nextLen, curLen) // 更新新序列的右端数字的value
     }
   }
   return max
