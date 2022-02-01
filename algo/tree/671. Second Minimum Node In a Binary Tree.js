c3bc068daf3fcda70c62d1a4d86115ce76a124c7
bianzheCN <fedfitz6@gmail.com>
2022-02-01 21:09:58 +0800

    [Tree Second Minimum Node In a Binary Tree]

--- algo/tree/671. Second Minimum Node In a Binary Tree.js
+++ algo/tree/671. Second Minimum Node In a Binary Tree.js
@@ -21,3 +21,33 @@ function findSecondMinimumValue(root) {
     dfs(root.right)
   }
 }
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var findSecondMinimumValue = function (root) {
+  let ans = -1
+
+  const dfs = (root, cur) => {
+    if (!root) return
+    if (root.val !== cur) {
+      if (ans === -1) ans = root.val
+      else ans = Math.min(ans, root.val)
+    }
+
+    root.left && dfs(root.left, cur)
+    root.right && dfs(root.right, cur)
+  }
+
+  dfs(root, root.val)
+  return ans
+}
