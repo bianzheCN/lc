684685384ba3eb942749d979a2ceaafa0aa278e4
bianzheCN <fedfitz6@gmail.com>
2022-02-21 20:54:18 +0800

    [binary] template

---
+++ algo/binary/template.js
@@ -0,0 +1,16 @@
+function binarySearch(arr, n, x) {
+  let head = 0,
+    tail = n - 1,
+    mid
+  while (head <= tail) {
+    mid = (head + tail) >> 1
+    if (arr[mid] === x) return mid
+    if (arr[mid] < x) {
+      head = mid + 1
+    } else {
+      tail = mid - 1
+    }
+  }
+
+  return -1
+}
