b86e4e652c8a14a1e737ca7d2e3492af0376da97
bianzheCN <fedfitz6@gmail.com>
2022-02-23 21:11:03 +0800

    [sliding window] minimum operations to reduce X to zero

---
+++ algo/sliding window/1658. Minimum Operations to Reduce X to Zero.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @param {number} x
+ * @return {number}
+ */
+var minOperations = function (nums, x) {
+  const n = nums.length
+  let left = 0,
+    right = 0,
+    maxLength = -1,
+    sum = 0
+  for (let i = 0; i < n; i++) {
+    sum += nums[i]
+  }
+  let count = sum - x
+  if (count < 0) {
+    return -1
+  }
+  let sumNums = 0
+  while (right < n) {
+    sumNums += nums[right]
+    while (sumNums > count) {
+      sumNums -= nums[left]
+      left++
+    }
+    if (sumNums == count) {
+      maxLength = Math.max(maxLength, right - left + 1)
+    }
+    right++
+  }
+  if (maxLength === -1) return -1
+  else {
+    return n - maxLength
+  }
+}
