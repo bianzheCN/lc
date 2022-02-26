3c995225158f8171b990c052155334eb1ddd5ff2
bianzheCN <fedfitz6@gmail.com>
2022-02-26 11:57:33 +0800

    [binary] search in rotated sorted array

--- algo/binary/33. Search in Rotated Sorted Array.js
+++ algo/binary/33. Search in Rotated Sorted Array.js
@@ -32,3 +32,44 @@ function find(arr, l, r, target) {
 
   return arr[r] === target ? r : -1
 }
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+// O(log(n))
+var search = function (nums, target) {
+  const n = nums.length
+  if (n == 0) return -1
+  if (n == 1) return nums[0] == target ? 0 : -1
+
+  let l = 0,
+    r = n - 1
+  while (l < r) {
+    const mid = (l + r + 1) >> 1
+    if (nums[mid] >= nums[0]) {
+      l = mid
+    } else {
+      r = mid - 1
+    }
+  }
+
+  if (target >= nums[0]) {
+    l = 0
+  } else {
+    l = l + 1
+    r = n - 1
+  }
+
+  while (l < r) {
+    const mid = (l + r) >> 1
+    if (nums[mid] >= target) {
+      r = mid
+    } else {
+      l = mid + 1
+    }
+  }
+
+  return nums[r] === target ? r : -1
+}
