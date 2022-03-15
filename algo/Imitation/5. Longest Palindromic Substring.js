88a70241623250b169a1735e279b40fbe3d18cc0
bianzheCN <fedfitz6@gmail.com>
2022-03-15 13:33:00 +0800

    [imitation] add explaination of Longest Palindromic Substring

--- algo/Imitation/5. Longest Palindromic Substring.js
+++ algo/Imitation/5. Longest Palindromic Substring.js
@@ -30,5 +30,13 @@ var longestPalindrome = function (s) {
  * I will expand the string from index i while i am traversing the original string param
  * before I do this, I wanna say that the expanding is actually should involve two scenarios,
  * one is that the result string is a odd-length, the other is even-length,
- * because for these two cases, the right border index of the string is gonna be different
+ * because for these two cases, the right border index of the string is gonna be different.
+ *
+ * Then it's about how to check if a sub-string is palindrome and compute the length of the answer which is the longest
+ * palindrome string.
+ * For checking the validness, I will decrement l and increment r when I am checking and after that return the valid string,
+ * For compute the length of answer, I will compare the answer I got with the new valid string, updating the answer if what I
+ * got is longer.
+ * 
+ * I this way, I believe it makes sense that I can get the right answer.
  */
